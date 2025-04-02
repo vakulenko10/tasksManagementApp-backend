d24ec7bb7f1cb980f7eaf6b19ace65575294f8c7
Vitalik Vakulenko <121180497+vakulenko10@users.noreply.github.com>
2025-04-02 02:39:34 +0200

    added Frontend orgigin for deployment version

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173', // set production origin via env
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
